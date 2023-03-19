97c7791c1fbab7bef64035591be05bc5ecd42497
Arshi Khwaja <arshikhwaja@gmail.com>
2023-03-19 20:17:03 +0530

    Pages and Components added

---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const About = () => {
+    return (
+        <h1>Ye mera  h</h1>
+    )
+}
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const Home = () => {
+    return (
+        <h1>Ye mera ghar h</h1>
+    )
+}
+export default Home;
